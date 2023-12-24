90327e617d762721a3240374edaa2a1410d121fb
TylerCharlton <TyCharlton1993@gmail.com>
2023-12-23 21:31:49 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1
+const divide = (num1, num2) => num1 / num2;
+
+const result = divide(2000, 100);
+console.log(result);
+
+// 2
+const square = (num) => num * num;
+
+const sqResult = square(2)
+console.log(sqResult);
+
+// 3
+const add = (num1, num2) => num1 + num2;
+
+const addResult = add(420, 69);
+console.log(addResult);
+
+
